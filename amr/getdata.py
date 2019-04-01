10ba1482d5f0dde20b13ef99c0144e6c8ad5f140
schultzm <dr.mark.schultz@gmail.com>
2019-04-01 16:30:08 +1100

    Parsed table

--- amr/getdata.py
+++ amr/getdata.py
@@ -8,7 +8,7 @@ from subprocess import Popen, PIPE
 import shlex
 import xml.etree.ElementTree as ET #https://docs.python.org/3.7/library/xml.etree.elementtree.html#module-xml.etree.ElementTree
 import pandas as pd
-from collections import defaultdict
+# from collections import defaultdict
 
 results = pd.DataFrame()
 cmd1 = "esearch -db BioProject -query"
@@ -19,12 +19,54 @@ proc3 = Popen(shlex.split(cmd3), stdin=proc1.stdout, stdout=PIPE, stderr=PIPE)
 result_masterproj = proc3.communicate()[0].decode("UTF-8")
 masterproject = ET.fromstring(result_masterproj)
 for child in masterproject:
-    result = defaultdict(list)
+    result = pd.DataFrame()
     for link in child.iter('ProjectIDRef'):
-        result['SlaveBioProject'].append(link.attrib['accession'])
-        result['MasterBioProject'].append(master_bioproject)
-    result = pd.DataFrame(result)
-    print(result)
+        # Get the metadata for the slave project
+        proc_a = Popen(shlex.split(f"{cmd1} {link.attrib['accession']}"),
+                       stdout=PIPE)
+        proc_b = Popen(shlex.split(cmd3), stdin=proc_a.stdout, stdout=PIPE,
+                       stderr=PIPE)
+        result_slaveproj = proc_b.communicate()[0].decode("UTF-8")
+        print(result_slaveproj)
+        slaveproject = ET.fromstring(result_slaveproj)
+        for grandchild in slaveproject:
+            title_ = None
+            for title in grandchild.iter('Title'):
+                title_ = title.text
+            for samn in grandchild.iter('LocusTagPrefix'):
+                # Add the accessions to the result table
+                grandchild_result = {}
+                grandchild_result['Title'] = title_
+                grandchild_result['SlaveBioProject'] = link.attrib['accession']
+                grandchild_result['MasterBioProject'] = master_bioproject
+                # print(samn.attrib)
+                grandchild_result.update(samn.attrib)
+                # print(grandchild_result)
+                grandchild_result = pd.DataFrame([grandchild_result])
+                # print(grandchild_result)
+                # print(grandchild_result)
+                results = pd.concat([grandchild_result, results],
+                                    ignore_index=True,
+                                    sort=False)
+                # proc_a = Popen(
+                #     shlex.split(f"{cmd1} {link.attrib['accession']}"),
+                #     stdout=PIPE)
+                # proc_b = Popen(shlex.split(cmd3), stdin=proc_a.stdout,
+                #                stdout=PIPE,
+                #                stderr=PIPE)
+
+                # result['BioSample'] = samn.attrib['biosample_id']
+
+                # for text in sublink.iter():
+                #     print(text)
+        #     print(help(grandchild))
+            # sys.exit()
+            # for docsum in grandchild.findall('DocumentSummary'):
+            #     print(docsum.attr)
+
+results.to_csv(SLAVEPRJs)
+    # result = pd.DataFrame(result)
+    # print(result)
             # print(grandchild.tag)
     # print(projectlinks.tag, projectlinks.attrib)
 
