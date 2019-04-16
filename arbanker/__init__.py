6964ba328d0e17005281505b14ee2fb2487036d7
schultzm <dr.mark.schultz@gmail.com>
2019-04-16 13:02:01 +1000

    Refactored to new repo name

---
+++ arbanker/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+__name__            = 'arbanker'
+__version__         = '0.1.0b'
+__version_date__    = 'Tue 16th April, 2019'
+__description__     = ('''ARBanker is a bioinformatics tool for 
+capturing metadata for isolates from the CDC AR Isolate Bank.
+Github repo and issue tracker:  https://github.com/schultzm/ARBanker/.
+''')
+__author__          = 'Mark B. Schultz'
+__author_email__    = 'dr.mark.schultz@gmail.com'
+__github_username__ = 'schultzm'
+__download_url__    = 'https://github.com/schultzm/ARBanker/zipball/master'
+__license__         = 'AGPL-3.0'
+__all__             = ['arbanker'] # This line generates a pylint error. Fix.
