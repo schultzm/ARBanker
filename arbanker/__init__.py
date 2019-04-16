f0cac502b7250557e26ea15849021e9498de86d2
schultzm <dr.mark.schultz@gmail.com>
2019-04-16 13:36:17 +1000

    Debuggery

--- arbanker/__init__.py
+++ arbanker/__init__.py
@@ -1,5 +1,24 @@
 # -*- coding: utf-8 -*-
+
+'''
+Uses python3.
+Email: dr.mark.schultz@gmail.com
+Github: schultzm
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 __name__            = 'arbanker'
+__parent_dir__      = 'arbanker'
 __version__         = '0.1.0b'
 __version_date__    = 'Tue 16th April, 2019'
 __description__     = ('''ARBanker is a bioinformatics tool for 
@@ -9,6 +28,7 @@ Github repo and issue tracker:  https://github.com/schultzm/ARBanker/.
 __author__          = 'Mark B. Schultz'
 __author_email__    = 'dr.mark.schultz@gmail.com'
 __github_username__ = 'schultzm'
+__install__         = 'pip3 install git+https://github.com/schultzm/ARBanker.git'
 __download_url__    = 'https://github.com/schultzm/ARBanker/zipball/master'
 __license__         = 'AGPL-3.0'
 __all__             = ['arbanker'] # This line generates a pylint error. Fix.
